f540177fb368cf35c4af7577b54d037f1f9ed52d
ThanhDuongg05 <dranky01@gmail.com>
2026-07-07 08:49:51 +0700

    Add files via upload

---
+++ ntd.js
@@ -0,0 +1,17 @@
+//ham tinh tuoi (lay nam sinh)
+function caculateAge(yearBorn){
+    var currentTime =new Date();
+    var currentYear = currentTime.getFullYear();
+    console.log(currentYear);
+}
+
+var name= prompt("Enter your name: ");
+console.log(name);
+
+
+//tinh nam sinh
+var yearBorn=prompt("Nhap nam sinh cua ban: ");
+
+var age;
+age=2026- yearBorn;
+console.log(age);
