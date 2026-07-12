43e9b79d04aec8984112ee291167c22a24547afc
ThanhDuongg05 <dranky01@gmail.com>
2026-07-12 09:24:22 +0700

    Add files via upload

--- confirm.js
+++ confirm.js
@@ -1,12 +1,28 @@
-function init(){
+function loadData()
+{
     var name=document.getElementById("confirm_name")
     var age=document.getElementById("confirm_age")
     var email=document.getElementById("confirm_email")
     var phone=document.getElementById("confirm_phone")
-    name.textContent = sessionStorage.firstname + " " + sessionStorage.lastname
-    age.textContent=sessionStorage.age
-    email.textContent=sessionStorage.email
-    phone.textContent=sessionStorage.phone
+    var occupation = document.getElementById("confirm_occupation")
+
+
+    name.textContent = sessionStorage.firstname + " " + sessionStorage.lastname;
+    age.textContent=sessionStorage.age;
+    email.textContent=sessionStorage.email;
+    phone.textContent=sessionStorage.phone;
+    occupation.textContent=sessionStorage.occupation;
+}
+
+function cancelButton()
+{
+    window.location ="ntd2.html";
+}
+
+function init(){
+    loadData();
+    var cancel = document.getElementById("cancel_button");
+    cancel.onclick = cancelButton;
 }
 
 window.onload=init;
--- ntd2.js
+++ ntd2.js
@@ -4,18 +4,95 @@ function saveData(){
     var lastname = document.getElementById("lastname").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phonenumber").value;
-    var age = document.getElementById("age").value;
+    var age = document.getElementById("age").value;  
     sessionStorage.firstname = firstname;
     sessionStorage.lastname = lastname;
     sessionStorage.email=email;
     sessionStorage.phone=phone;
     sessionStorage.age=age;
+
+    var highschool =document.getElementById("highschool").checked;
+    var  university =document.getElementById("university").checked;
+    var  working =document.getElementById("working").checked;
+    var  retired =document.getElementById("retired").checked;
+
+    if(highschool==true){
+        sessionStorage.occupation="highschool";
+    }
+    if(university==true){
+        sessionStorage.occupation="university";
+    }
+    if(working==true){
+        sessionStorage.occupation="working";
+    }
+    if(retired==true){
+        sessionStorage.occupation="retired";
+    }
+}
+//ham ko xoa het du lieu
+function prefillData(){
+    if (sessionStorage.firstname != null) {
+        document.getElementById("firstname").value =sessionStorage.firstname;
+        document.getElementById("lastname").value =sessionStorage.lastname;
+        document.getElementById("email").value =sessionStorage.email;
+        document.getElementById("phonenumber").value =sessionStorage.phone;
+        document.getElementById("age").value =sessionStorage.age;
+        
+         ntd2.js
+        //C1
+        // if(sessionStorage.occupation=="highshcool"){
+        //    sessionStorage.occupation= document.getElementById("highschool").value;
+        // }
+        // if(sessionStorage.occupation=="university"){
+        //     sessionStorage.occupation=document.getElementById("university").value;
+        // }
+        // if(sessionStorage.occupation=="working"){
+        //     sessionStorage.occupation=document.getElementById("working").value;
+        // }
+        // if(sessionStorage.occupation=="retired"){
+        //     sessionStorage.occupation=document.getElementById("retired").value;
+        // }
+
+        //c2
+        // switch (sessionStorage.occupation)
+        // {
+        //     case "highschool":
+        //         document.getElementById("highschool").checked=true;
+        //         break;
+        //     case "university":
+        //         document.getElementById("university").checked=true;
+        //         break;
+        //     case "working":
+        //         document.getElementById("working").checked=true;
+        //         break;
+        //     case "retired":
+        //         document.getElementById("retired").checked=true;
+        //         break;
+        var occupationArray=document.getElementById("radio_occupation").getElementsByTagName("input");
+        //c3
+        // var i=0;
+        // while(i<occupationArray.length){
+        //     if(occupationArray[i].checked == true){
+        //         sessionStorage.occupation=occupationArray[i].value;
+        //     }
+        //     i+=1;
+        // }
+        // c4
+        for(var i=0;i<occupationArray.length;i++)
+        {
+            if(occupationArray[i].checked==true)
+            {
+                sessionStorage.occupation=occupationArray[i].value;
+            }
+        }
+    }
 }
 
 function init(){
     // tao form dang ky
     var regForm = document.getElementById("ntd2");
     regForm.onsubmit=saveData;
+    prefillData();
 }
 
 window.onload =init;
